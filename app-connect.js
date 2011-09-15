ca3f99434f5c6a82984064fe5fa9998ce9aba089
Doug Clelland <doug@Doug-Clellands-iMac.local>
2011-09-15 16:49:30 -0600

    added nodester port

--- app-connect.js
+++ app-connect.js
@@ -16,5 +16,5 @@ connect.createServer()
                 	require('./fibo2-node').get);
                 app.get('/mult', htutil.loadParams,
                 	require('./mult-node').get);
-        })).listen(process.env['app_port']);
-console.log('listening to http://localhost:8124');
+        })).listen(11952);
+console.log('listening to http://localhost:11952');
